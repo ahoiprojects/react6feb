dcf61c8bf8a894ad0ba69225231646a5391f7bdc
Nehadevops12 <99031601+Nehadevops12@users.noreply.github.com>
2022-02-19 20:33:03 +0530

    chagne in appl.sasdf

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload direct changes by ramandeep kaur.
+          Edit <code>src/App.js</code> and save to reload direct changes by ramandeep kaur inise app.js - with load balancer chadfa.
         </p>
         <a
           className="App-link"

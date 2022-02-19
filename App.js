b08aab7f5599d1a3cace2bc7acdb67a7dbf29bd7
Nehadevops12 <99031601+Nehadevops12@users.noreply.github.com>
2022-02-19 20:32:30 +0530

    change in file

--- App.js
+++ App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload RAMANDEEP KAUR.
+          Edit <code>src/App.js</code> and save to reload RAMANDEEP KAUR with load balanceer check.
         </p>
         <a
           className="App-link"
